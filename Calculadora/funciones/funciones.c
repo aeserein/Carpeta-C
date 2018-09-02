ef07cb585c44ff8344ad8954114af1669098df5d
aeserein <ae.serein@gmail.com>
2018-09-02 04:11:02 -0300

    02/09

---
+++ Calculadora/funciones/funciones.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctype.h>
+
+
+float sumarDos(float n1, float n2) {
+    float resultado;
+    resultado = n1+n2;
+    return resultado;
+}
+
+float restarDos(float n1, float n2) {
+    float resultado;
+    resultado = n1-n2;
+    return resultado;
+}
+
+float multiplicarDos(float n1, float n2) {
+    float resultado;
+    resultado = n1*n2;
+    return resultado;
+}
+
+float dividirDos(float n1, float n2) {
+    float resultado;
+    resultado = n1/n2;
+    return resultado;
+}
+
+float factorial(float n) {
+    float resultado;
+    if (n==1) {
+        return 1;
+    } else {
+        resultado = n*factorial(n-1);
+    }
+    return resultado;
+}
+
+float ingresarNumero(void) {
+    float n;
+    printf("Ingrese un n%cmero:\t" , 163);
+    scanf("%f" , &n);
+    return n;
+}
+
+void pausaYClear(void) {
+    system("pause");
+    system("cls");
+}
+
+int continuar(void) {
+    char letra;
+    do {
+        printf("%cContinuar? S/N\t\t" , 168);
+        letra = getche();
+        letra = toupper(letra);
+    } while (letra!='S' && letra!='N');
+
+    return (letra=='S');
+}
--- Calculadora/main.c
+++ Calculadora/main.c
@@ -1,49 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <D:\Alvaro\Trabajos\Programación\01 - Programación 1\Calculadora\funciones\funciones.c>
 
 int main() {
 
-    printf("CALCULADORA\n-----------\n\n");
-    int opcion;
+    printf("CALCULADORA\n-----------------\n\n");
+    unsigned short opcion;
+    float n1;
+    float n2;
+    float resultado1;
+    float resultado2;
+
     do {
         printf("1. Sumar\n");
         printf("2. Restar\n");
         printf("3. Multiplicar\n");
         printf("4. Dividir\n");
-        printf("5. Salir\n");
-        printf("Ingrese una opción:\t");
-        scanf("%d" , &opcion);
-        printf("\n");
+        printf("5. Factorial\n");
+        printf("-----------------\n");
+        printf("6. Salir\n\n");
+        opcion = ingresarNumero();
 
         switch (opcion) {
             case 1 : {
-                // funcion sumar
+                n1 = ingresarNumero();
+                n2 = ingresarNumero();
+                resultado1 = sumarDos(n1,n2);
+                printf("\nSUMA:\t%.2f\n" , resultado1);
                 break;
             }
             case 2 : {
-                // funcion restar
+                n1 = ingresarNumero();
+                n2 = ingresarNumero();
+                resultado1 = restarDos(n1,n2);
+                printf("\nRESTA:\t%.2f\n" , resultado1);
                 break;
             }
             case 3 : {
-                // funcion multiplicar
+                n1 = ingresarNumero();
+                n2 = ingresarNumero();
+                resultado1 = multiplicarDos(n1,n2);
+                printf("PRODUCTO:\t%.2f\n" , resultado1);
                 break;
             }
             case 4 : {
-                // funcion dividir
+                n1 = ingresarNumero();
+                n2 = ingresarNumero();
+                resultado1 = dividirDos(n1,n2);
+                printf("\nDIVISI%cN:\t%.2f\n" , 224 , resultado1);
                 break;
             }
             case 5 : {
-                break; //si es 5 sale
+                n1 = ingresarNumero();
+                n2 = ingresarNumero();
+                resultado1 = factorial(n1);
+                resultado2 = factorial(n2);
+                printf("\nFACTORIAL 1:\t%.2f\n" , resultado1);
+                printf("\nFACTORIAL 2:\t%.2f\n" , resultado2);
+                break;
+            }
+            case 6 : {
+                break;
             }
             default : {
-                printf("Opci%cn inv%clida" , 162 , 160);
+                printf("\nOpci%cn inv%clida\n" , 162 , 160);
             }
-        } // TODO ESTO EN UNA FUNCION
+        }
 
-        system("pause");
-        system("cls");
+        if (opcion!=6) {
+            printf("\n");
+            if (!continuar()) {
+                printf("\n");
+                return 0;
+            }
+            system("cls");
+        }
 
-    } while (opcion!=5);
+
+    } while (opcion!=6);
 
     return 0;
 
